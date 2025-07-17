899aecc26d0de58c68e348c0d974660e5fdfa9a0
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-07-17 15:11:09 +0300

    Implement simple express error handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from "express";
 import { connectDB } from "./db/connectDB.js";
 import userRoutes from "./routes/userRoutes.js";
 import scoreRoutes from "./routes/scoreRoutes.js";
+import { ApiError } from "./utils/apiErrors.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,8 +12,16 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/scores", scoreRoutes);
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  const response = { status: "error", message: err.message };
-  res.status(err.status || 500).json(response);
+  let response: { status: string; message: string };
+  if (err instanceof ApiError) {
+    err.log();
+    response = { status: "error", message: err.message };
+    res.status(err.status).json(response);
+  } else {
+    console.error("Error: ", err?.message);
+    response = { status: "error", message: "Unknown exception" };
+    res.status(500).json(response);
+  }
 });
 
 (async () => {
