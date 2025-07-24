4e3e01da9bc55242fa52120bfe229d14a963afff
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-07-24 19:07:47 +0300

    Update LeaderboardView to display leaderboard entries

--- web/src/components/leaderboard/LeaderboardView.vue
+++ web/src/components/leaderboard/LeaderboardView.vue
@@ -1,16 +1,50 @@
 <script setup lang="ts">
 import { useCategoryStore } from "@/stores/categoryStore";
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
+interface LeaderboardUser {
+  _id: string;
+  username: string;
+}
+interface LeaderboardEntry {
+  _id: string;
+  user: LeaderboardUser;
+  gamemode: string;
+  map: string;
+  score: number;
+  timestamp: string;
+}
 const props = defineProps<{ gamemode: string; map: string }>();
+const leaderboard = ref<LeaderboardEntry[]>([]);
 const store = useCategoryStore();
-onMounted(() => {
+
+onMounted(async () => {
   store.fetchCategories();
+  const response = await fetch(`/api/scores/${props.gamemode}/${props.map}`);
+  if (response.ok) {
+    leaderboard.value = (await response.json()).data;
+  }
 });
+
 const gamemodeObj = computed(() => store.getGamemodeById(props.gamemode));
 const mapObj = computed(() => store.getMapById(props.map));
 </script>
 <template>
-  <h2>Leaderboard view for:</h2>
-  <p>Gamemode: {{ gamemodeObj?.name || props.gamemode }}</p>
-  <p>Map: {{ mapObj?.name || props.map }}</p>
+  <p><b>Gamemode: </b>{{ gamemodeObj?.name || props.gamemode }}</p>
+  <p><b>Map: </b>{{ mapObj?.name || props.map }}</p>
+  <table>
+    <thead>
+      <tr>
+        <th>Username</th>
+        <th>Score</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="entry in leaderboard" :key="entry._id">
+        <td>{{ entry.user.username }}</td>
+        <td>{{ entry.score }}</td>
+        <td>{{ new Date(entry.timestamp).toLocaleString() }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
