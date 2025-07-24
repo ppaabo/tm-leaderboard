2abc860db04d0fa7c84321a93a16da36db7dd872
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-07-24 13:51:01 +0300

    Update vite and ts configs

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
   server: {
     proxy: {
       "/api": {
-        target: "http://backend:8080"
-      }
-    }
-  }
-})
+        target: "http://backend:8080",
+      },
+    },
+  },
+});
