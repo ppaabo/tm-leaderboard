947eab48be8b102c72076d8712a96bc5be766680
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-07-25 21:16:10 +0300

    Add PicoCSS, update components to use pico

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view />
+  <header>
+    <h4>Header</h4>
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <h4>Footer</h4>
+  </footer>
 </template>
 
 <style scoped></style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router.ts";
-import "./style.css";
 import App from "./App.vue";
+import "./pico-custom.scss";
 
 const pinia = createPinia();
 const app = createApp(App);
