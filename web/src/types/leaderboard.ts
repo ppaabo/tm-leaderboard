c3bdd2eebde24f7014122ed63a1632454585ce32
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-07-28 18:39:53 +0300

    Add leaderboard type definitions

---
+++ web/src/types/leaderboard.ts
@@ -0,0 +1,19 @@
+export interface LeaderboardUser {
+  _id: string;
+  username: string;
+}
+
+export interface LeaderboardEntryData {
+  _id: string;
+  user: LeaderboardUser;
+  gamemode: string;
+  map: string;
+  score: number;
+  timestamp: string;
+}
+
+export interface LeaderboardEntryDisplay
+  extends Omit<LeaderboardEntryData, "score"> {
+  score: string;
+  rawScore?: number;
+}
