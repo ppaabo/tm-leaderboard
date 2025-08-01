d396ba76ab1235e47a7ceb422fa0686f7af070b7
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-08-01 21:10:14 +0300

    Implement LoginView component and authStore method for user login

--- web/src/components/user/LoginView.vue
+++ web/src/components/user/LoginView.vue
@@ -1,11 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useAuthStore } from "@/stores/auth-store";
+import type { LoginPayload } from "@/types";
+
+const authStore = useAuthStore();
+const usernameInput = ref("");
+const passwordInput = ref("");
+
+const loginUser = () => {
+  const user: LoginPayload = {
+    username: usernameInput.value,
+    password: passwordInput.value,
+  };
+  authStore.loginUser(user);
+};
+</script>
 
 <template>
-  <b>Login page</b>
-  <p>Don't have an account?</p>
-  <router-link :to="{ name: 'signup' }">
-    <button>Create account</button>
-  </router-link>
+  <article>
+    <body>
+      <form @submit.prevent="loginUser">
+        <fieldset>
+          <label>
+            Username
+            <input
+              v-model.trim="usernameInput"
+              name="first_name"
+              placeholder="username"
+              autocomplete="username"
+            />
+          </label>
+          <label>
+            Password
+            <input
+              v-model.trim="passwordInput"
+              type="password"
+              name="email"
+              placeholder="password"
+              autocomplete="current-password"
+            />
+          </label>
+        </fieldset>
+        <button type="submit">Login</button>
+      </form>
+    </body>
+    <footer>
+      <p>Don't have an account?</p>
+      <router-link :to="{ name: 'signup' }">
+        <button>Create account</button>
+      </router-link>
+    </footer>
+  </article>
 </template>
 
 <style scoped></style>
--- web/src/stores/auth-store.ts
+++ web/src/stores/auth-store.ts
@@ -6,7 +6,27 @@ export const useAuthStore = defineStore("auth", () => {
   const currentUser = ref<AuthUser | null>(null);
 
   async function loginUser(userObj: LoginPayload) {
-    console.log(userObj);
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userObj),
+        }
+      );
+      if (response.ok) {
+        const data: AuthUser = (await response.json()).data;
+        currentUser.value = data;
+      } else {
+        throw new Error(`Response status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("loginUser", error);
+    }
   }
 
   async function createUser(userObj: RegisterPayload) {
