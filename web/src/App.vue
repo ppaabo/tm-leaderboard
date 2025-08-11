502d899e97394584868884ba23759fb147a9409f
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-08-11 16:00:17 +0300

    Add vue-notifications, add custom styling for notifications

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,7 +2,6 @@
 import { onMounted } from "vue";
 import { useAuthStore } from "./stores/auth-store";
 import NavBar from "./components/NavBar.vue";
-
 const authStore = useAuthStore();
 onMounted(() => {
   authStore.refreshSession();
@@ -11,6 +10,7 @@ onMounted(() => {
 
 <template>
   <header>
+    <notifications classes="custom-notification" position="top center" />
     <NavBar />
   </header>
   <main>
@@ -21,4 +21,56 @@ onMounted(() => {
   </footer>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.vue-notification-group {
+  padding-top: 1rem;
+}
+
+.custom-notification {
+  margin: 0 5px 5px;
+  padding: 10px;
+  font-size: 12px;
+  color: #ffffff;
+  border-radius: 0.2rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-left: 5px solid;
+  position: relative;
+  cursor: pointer;
+
+  .notification-title {
+    font-weight: bold;
+    padding-right: 20px;
+  }
+
+  &.success {
+    background: var(--pico-color-jade-700);
+    border-left-color: var(--pico-color-jade-500);
+  }
+
+  &.error {
+    background: var(--pico-color-red-700);
+    border-left-color: var(--pico-color-red-500);
+  }
+  &.info {
+    background: var(--pico-color-cyan-700);
+    border-left-color: var(--pico-color-cyan-500);
+  }
+
+  /* close button */
+  &::after {
+    content: "×";
+    position: absolute;
+    top: 6px;
+    right: 10px;
+    font-size: 16px;
+    font-weight: bold;
+    opacity: 0.7;
+    cursor: pointer;
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+</style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router.ts";
 import App from "./App.vue";
+import Notifications from "@kyvg/vue3-notification";
 import "./pico-custom.scss";
 
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia).use(router).mount("#app");
+app.use(pinia).use(router).use(Notifications).mount("#app");
