187f497a339010de1d953bc805e78d6c55563d5e
ppaabo <89841937+ppaabo@users.noreply.github.com>
2026-04-18 16:46:12 +0300

    add type for delete score status

--- web/src/types/score.ts
+++ web/src/types/score.ts
@@ -6,3 +6,13 @@ export interface ScoreValidationResult {
   isValid: boolean;
   value: number | null;
 }
+
+export const DeleteOwnScoreStatus = {
+  Deleted: "deleted",
+  NotFound: "notFound",
+  Forbidden: "forbidden",
+  Error: "error",
+} as const;
+
+export type DeleteOwnScoreStatus =
+  (typeof DeleteOwnScoreStatus)[keyof typeof DeleteOwnScoreStatus];
