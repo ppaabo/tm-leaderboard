63abbfdbf8a78fe06976f3b7008ae4f561991c3f
ppaabo <89841937+ppaabo@users.noreply.github.com>
2026-05-09 18:16:12 +0300

    api: call validateRouteParams for relevant user routes

--- api/src/routes/user-routes.ts
+++ api/src/routes/user-routes.ts
@@ -1,12 +1,19 @@
-import { Router } from "express";
-import { requireAuth, requireRole } from "@/middleware/auth-middleware.js";
 import userController from "@/controllers/user-controller.js";
+import { requireAuth, requireRole } from "@/middleware/auth-middleware.js";
+import { validateRouteParams } from "@/middleware/validate-request.js";
+import { zUsername, zObjectId } from "@/schemas";
+import { Router } from "express";
+import { z } from "zod";
 
 const router = Router();
 
 // router.get("/:id", userController.getUserProfile);
 router.delete("/me", requireAuth, userController.deleteOwnAccount);
-router.get("/:username/scores", userController.getUserScoresWithPlacement);
+router.get(
+  "/:username/scores",
+  validateRouteParams(z.object({ username: zUsername })),
+  userController.getUserScoresWithPlacement,
+);
 
 // Admin routes
 router.get(
@@ -20,6 +27,7 @@ router.delete(
   "/:id",
   requireAuth,
   requireRole(["admin"]),
+  validateRouteParams(z.object({ id: zObjectId })),
   userController.deleteUserAccount,
 );
 
--- api/src/schemas/user.ts
+++ api/src/schemas/user.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
+export const zUsername = z
+  .string()
+  .min(4, { error: "must be at least 4 characters" })
+  .max(32, { error: "must be at most 32 characters" });
+
 export const signUpSchema = z.strictObject({
-  username: z
-    .string()
-    .min(4, { error: "must be at least 4 characters" })
-    .max(32, { error: "must be at most 32 characters" }),
+  username: zUsername,
   email: z.email({ error: "Invalid email address" }),
   password: z.string().min(8, { error: "must be at least 8 characters" }),
 });
