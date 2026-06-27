ba6f3d05ee6a0511f4cc5aa2501e5543d2a5fa2a
ppaabo <89841937+ppaabo@users.noreply.github.com>
2026-06-27 18:51:41 +0300

    Update UserSettingsView to use ConfirmDialog

--- web/src/components/user/UserSettingsView.vue
+++ web/src/components/user/UserSettingsView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
+import ConfirmDialog from "@/components/ConfirmDialog.vue";
 import { useUserStore } from "@/stores/user-store";
-import { useRouter } from "vue-router";
 import { ref } from "vue";
-import LoadingIndicator from "@/components/LoadingIndicator.vue";
+import { useRouter } from "vue-router";
 
 const userStore = useUserStore();
 const router = useRouter();
@@ -34,29 +34,17 @@ const cancelDelete = () => {
   <hr />
   <h2>Account Settings</h2>
   <button @click="openConfirmModal">Delete Account</button>
-  <dialog :open="showConfirmModal">
-    <article>
-      <header>
-        <h2>Confirm Account Deletion</h2>
-      </header>
-      <p>
-        Are you sure you want to delete your account? This action cannot be
-        undone.
-      </p>
-      <p>
-        Deleting your account also deletes all your scores from the leaderboards
-      </p>
-      <LoadingIndicator
-        v-if="isDeleting"
-        inline
-        message="Deleting account..."
-      />
-      <footer>
-        <button class="secondary" @click="cancelDelete" :disabled="isDeleting">
-          Cancel
-        </button>
-        <button @click="confirmDelete" :disabled="isDeleting">Confirm</button>
-      </footer>
-    </article>
-  </dialog>
+  <ConfirmDialog
+    v-model:open="showConfirmModal"
+    title="Confirm Account Deletion"
+    message="Are you sure you want to delete your account? This action cannot be undone."
+    :loading="isDeleting"
+    loadingMessage="Deleting account..."
+    @cancel="cancelDelete"
+    @confirm="confirmDelete"
+  >
+    <p>
+      Deleting your account also deletes all your scores from the leaderboards
+    </p>
+  </ConfirmDialog>
 </template>
